695974db26b0d1a867d8cd78f6c4e98c966cf4d8
RONGAMES2 <adrianighere@gmail.com>
2026-02-16 10:37:13 -0700

    Base Level(UNFINISHED)

--- metroidvania-game/Scripts/player.gd
+++ metroidvania-game/Scripts/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var jump_sound: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -750.0
@@ -24,12 +26,13 @@ func _physics_process(delta: float) -> void:
 		animated_sprite_2d.animation = "Falling"
 	
 	# Add the gravity.
-	if not is_on_floor():
+	if not is_on_floor() or Input.is_action_just_released("jump"):
 		velocity += get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_sound.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
