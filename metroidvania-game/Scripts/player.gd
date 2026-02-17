bb942e57aafd7bfd3d9be924c3c8ad754feab880
RONGAMES2 <adrianighere@gmail.com>
2026-02-16 17:52:30 -0700

    Fixed Jumping & Double Jump Added

--- metroidvania-game/Scripts/player.gd
+++ metroidvania-game/Scripts/player.gd
@@ -3,13 +3,22 @@ extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var jump_sound: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
+#constant variables
+const SPEED := 300.0
+const GRAVITY := 1000
+const FALL_GRAVITY := 1500
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -750.0
+#variables
+var JUMP_VELOCITY = -550
+var jump_amount = 0
 
+
+func gravity(velocity: Vector2):
+	if velocity.y < 0:
+		return GRAVITY
+	return FALL_GRAVITY
 
 func _physics_process(delta: float) -> void:
-	
 	# Add animation
 	if velocity.x > 1 or velocity.x < -1:
 		animated_sprite_2d.animation = "Running"
@@ -25,14 +34,28 @@ func _physics_process(delta: float) -> void:
 	elif velocity.y > 0:
 		animated_sprite_2d.animation = "Falling"
 	
+	if is_on_floor():
+		jump_amount = 0
+	
+	
 	# Add the gravity.
 	if not is_on_floor() or Input.is_action_just_released("jump"):
-		velocity += get_gravity() * delta
+		velocity.y += gravity(velocity) * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		jump_sound.play()
+	
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y = JUMP_VELOCITY / 4
+	
+	if !is_on_floor() and !jump_amount == 2:
+			if Input.is_action_just_pressed("jump"):
+				velocity.y = JUMP_VELOCITY
+				jump_amount += 1
+			if Input.is_action_just_released("jump") and velocity.y < 0:
+				velocity.y = JUMP_VELOCITY / 4
+				jump_amount += 1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
